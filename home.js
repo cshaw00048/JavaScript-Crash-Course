5bc7c7227b4b25ff07199ee1ea49514e1625c5c7
Christian Shaw <cshaw00048@gmail.com>
2021-04-20 06:20:12 -0500

    basics of JS

--- home.js
+++ home.js
@@ -1,2 +1,16 @@
 console.log("hello");
-alert("yooo!");
+//alert("yooo!");
+
+// How to write a comment inline
+
+//Variables
+
+var b = "smoothie";
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var age = prompt("What is your age?");
+
+document.getElementById("someText").innerHTML = age;
